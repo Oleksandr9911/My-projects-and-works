66ed25bbec874f8262fafa7a92a3df6256c8d230
Oleksandr9911 <rudiako99@gmail.com>
2024-08-28 17:00:04 +0200

    Delete if-else-exist.bat

--- if-else-exist.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-
-rem | Сповiшення за умовою. Ключ exist перев?ряє чи ?снує файл
-
-if /i exist %1 (echo обєкт знайдено) else (echo обєкта не iснує)
-rem if /i not exist %1 (echo обєкт знайдено) else (echo обєкта не iснує)
-pause
