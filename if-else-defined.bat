bae7fd222df2a334cba025c66fc84b6a2a0a5db5
Oleksandr9911 <rudiako99@gmail.com>
2024-08-28 16:59:46 +0200

    Delete if-else-defined.bat

--- if-else-defined.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cls
-
-rem | Сповiшення за умовою. Ключ defined визначає зм?нну оточення
-
-if /i defined %1 (echo змiнну %1 визначено) else (echo змiнну %1 невизначено)
-rem if /i not defined %1 (echo змiнну %1 визначено) else (echo змiнну %1 невизначено)
-pause
